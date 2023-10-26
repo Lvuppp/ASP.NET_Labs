f394d8bd56ca1f53250ea1893f12fbe7cbe898d7
egor_bagrovets <egor.bagrovets.eb@gmail.com>
2023-10-26 22:09:51 +0300

    endLab7

---
+++ Web_153504.IdentityServer/Controller/AvatarController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_153504.IdentityServer.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AvatarController : ControllerBase
+    {
+    }
+}
